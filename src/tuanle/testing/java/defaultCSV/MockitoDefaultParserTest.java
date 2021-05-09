a93d31e8666dabcb15b16f17fa44f768de6f4a1d
Minh Tuan <minhtuanqn320@gmail.com>
2021-05-10 00:58:50 +0700

    update

--- src/tuanle/testing/java/defaultCSV/MockitoDefaultParserTest.java
+++ src/tuanle/testing/java/defaultCSV/MockitoDefaultParserTest.java
@@ -8,8 +8,11 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
+
+import static defaultCSV.CsvTestUtils.createCsvFileConfig;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MockitoDefaultParserTest {
@@ -48,6 +51,29 @@ public class MockitoDefaultParserTest {
                 .atSegment(1, "Trump")
                 .and().assertCsvLine(defaultCsvParserTest);
 
+    }
+
+    @Test
+    public void whenParse_UnquotedCsv_ThenCorrectlyParse() throws URISyntaxException, IOException {
+        final CsvFileConfig parserConfig = createCsvFileConfig();
+
+        File file = createFile("/unquoted.csv");
+
+        Mockito.when(fileConfig.getDelimiter()).thenReturn("|");
+        Mockito.when(fileConfig.isQuoteMote()).thenReturn(false);
+        DefaultCsvParser defaultCsvParserTest = new DefaultCsvParser(file, fileConfig);
+
+        CsvLinesAssertion
+                .create().expectLine(0)
+                .atSegment(0, "FirstName")
+                .atSegment(1, "LastName")
+                .and().expectLine(1)
+                .atSegment(0, "John")
+                .atSegment(1, "Biden")
+                .and().expectLine(2)
+                .atSegment(0, "Donald")
+                .atSegment(1, "Trump")
+                .and().assertCsvLine(defaultCsvParserTest);
     }
 
 
